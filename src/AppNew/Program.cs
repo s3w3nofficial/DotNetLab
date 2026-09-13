dbffdd791439673c3aa8b61b14c59ac222b6acef
s3w3nofficial <s3w3nofficial@gmail.com>
2026-09-13 21:43:30 +0200

    app new initial setup

---
+++ src/AppNew/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DotNetLab;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddFluentUIComponents();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+await builder.Build().RunAsync();
